eb5eca86610df7d6d1f94eee2c7a3cf83db4841c
Notevill <Notevill@gmail.com>
2019-10-11 17:36:54 +0500

    home notebook: create some new settings

--- i3/lounchpolybar.sh
+++ i3/lounchpolybar.sh
@@ -12,7 +12,14 @@ export SECMON=$(GetMonitor.sh sec)
 
 # Launch bar top
 polybar top &
-polybar network_bar &
+
+# in home notebook we start wifi bar
+if [[ $USER = 'bugdruhman' ]] ; then
+    polybar network_wifi_bar &
+else
+    polybar network_bar &
+fi
+
 polybar system_stats_bar &
 
 echo "Bars launched..."
